f22d3cf267db2cdadf42850c9eae76e00920a64b
nitunwa <nitun.2@gmail.com>
2025-05-24 15:01:46 -0700

    first commit

---
+++ src/main/java/com/nextgen/data/platform/datafoundation/App.java
@@ -0,0 +1,13 @@
+package com.nextgen.data.platform.datafoundation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
